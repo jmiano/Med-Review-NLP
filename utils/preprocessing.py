542c54045db2a75ca9ef4e870cc1926e1fc8b643
jmiano <j.miano@outlook.com>
2021-12-01 21:46:28 -0500

    improved text cleaning and added preprocessed columns

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -6,7 +6,10 @@ import numpy as np
 
 def clean_reviews(review):
     clean_review = BeautifulSoup(review, features='lxml')
-    return clean_review.get_text().replace('"', "")
+    clean_review = clean_review.get_text().replace('"', "")
+    clean_review = clean_review.replace('\n', ' ').replace('\r', '').replace('\t', '')
+    clean_review = clean_review.replace('\s{2,}', ' ')
+    return clean_review
 
 
 def load_data(file_path):
@@ -24,6 +27,21 @@ def load_data(file_path):
     with np.errstate(divide='ignore', invalid='ignore'):
         df['usefulScore'] = np.log(df['usefulCount']) / np.max(np.log(df['usefulCount']))
     df['usefulScore'] = df['usefulScore'].replace(-np.Inf, 0)
+
+    # Create normalized rating (0 to 1) to be used as a metadata feature
+    df['ratingNormalized'] = df['rating'] / np.max(df['rating'])
+
+    # Cast the date column to be a date datatype and compute the review age (with 0 corresponding to the most recent review)
+    df['date'] = pd.to_datetime(df['date'])
+    df['daysOld'] = (max(df['date']) - df['date']).astype('timedelta64[s]') / (60*60*24)
+
+    # Compute an age score as the log of daysOld normalized to be between 0 and 1
+    with np.errstate(divide='ignore', invalid='ignore'):
+        df['ageScore'] = np.log(df['daysOld']) / np.max(np.log(df['daysOld']))
+    df['ageScore'] = df['ageScore'].replace(-np.Inf, 0)
+
+    # Create onehot encoding for the condition so we can use it as a feature as well
+    df = pd.concat([df, pd.get_dummies(df['condition'])], axis=1)
 
     # Split data into train and val
     train = df.sample(frac=0.75, random_state=8)
