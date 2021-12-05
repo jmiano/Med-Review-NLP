7de49024a9092d15284e3d8771d1cfa75bc5128c
jmiano <j.miano@outlook.com>
2021-12-04 19:01:21 -0500

    added metadata-only neural network

--- models/transformer_models.py
+++ models/transformer_models.py
@@ -28,7 +28,6 @@ class UsefulScoreRegressorTextOnly(nn.Module):
         return logits
 
 
-
 class UsefulScoreRegressorAllFeat(nn.Module):
     def __init__(self, encoder, hidden_dim=768, num_meta_feats=0, outputs=1, dropout=0.1):
         super().__init__()
@@ -48,6 +47,29 @@ class UsefulScoreRegressorAllFeat(nn.Module):
         output = self.encoder(tokens, attention_mask)
         output = torch.cat([output.last_hidden_state[:, 0], meta_feats], dim=1)
         logits = self.head_lin1(output)
+        logits = self.lrelu(logits)
+        logits = self.dropout(logits)
+        logits = self.head_lin2(logits)
+
+        return logits
+
+
+class UsefulScoreRegressorMetaOnly(nn.Module):
+    def __init__(self, num_meta_feats=0, outputs=1, dropout=0.1):
+        super().__init__()
+
+        # Initializaton
+        self.hidden_dim = num_meta_feats
+        
+        # Head Architecture
+        self.head_lin1 = nn.Linear(in_features=self.hidden_dim, out_features=int(self.hidden_dim))
+        self.lrelu = nn.LeakyReLU()
+        self.dropout = nn.Dropout(dropout)
+        self.head_lin2 = nn.Linear(in_features=int(self.hidden_dim), out_features=outputs)
+
+
+    def forward(self, meta_feats):
+        logits = self.head_lin1(meta_feats)
         logits = self.lrelu(logits)
         logits = self.dropout(logits)
         logits = self.head_lin2(logits)
