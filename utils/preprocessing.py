1cc52292c9c3a06821c6187c903185900dab5e0f
jmiano <j.miano@outlook.com>
2021-12-07 20:41:56 -0500

    updated preprocessing for quantiles and classification

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -12,20 +12,24 @@ def clean_reviews(review):
     return clean_review
 
 
-def load_data(file_path, year_range=[2008, 2017], usefulCount_range=[0, 10000], usefulCount_quantile=None):
+def load_data(file_path, year_range=[2008, 2017], usefulCount_range=[0, 10000], usefulCount_quantile=None,
+              quantiles_for_class=[0.25, 0.5, 0.75]):
     # Read CSV
     df = pd.read_csv(file_path, encoding='utf-8')
 
     # Remove duplicate reviews
     df = df.drop_duplicates(subset=['review', 'condition', 'date', 'rating', 'usefulCount'])
 
+    # Get most common conditions (by review count)
+    top_conditions = list(df.groupby('condition').count().reset_index().sort_values(by='uniqueID', ascending=False)[:10]['condition'])
+    df = df.loc[df['condition'].isin(top_conditions)]
+
     # Filter the reviews by the input year_range
     df['date'] = pd.to_datetime(df['date'])
     df = df.loc[(df.date.dt.year >= year_range[0]) & (df.date.dt.year <= year_range[1]), :]
 
-    # Get most common conditions (by review count)
-    top_conditions = list(df.groupby('condition').count().reset_index().sort_values(by='uniqueID', ascending=False)[:10]['condition'])
-    df = df.loc[df['condition'].isin(top_conditions)]
+    # Create onehot encoding for the condition so we can use it as a feature as well
+    df = pd.concat([df, pd.get_dummies(df['condition'])], axis=1)
 
     # Clean review text
     df['cleanReview'] = df['review'].apply(clean_reviews)
@@ -49,14 +53,15 @@ def load_data(file_path, year_range=[2008, 2017], usefulCount_range=[0, 10000], 
     # Cast the date column to be a date datatype and compute the review age (with 0 corresponding to the most recent review)
     df['daysOld'] = (max(df['date']) - df['date']).astype('timedelta64[s]') / (60*60*24)
 
-    # Compute an age score as the log of daysOld normalized to be between 0 and 1
+    # Compute an age score as daysOld normalized to be between 0 and 1
     with np.errstate(divide='ignore', invalid='ignore'):
         df['ageScore'] = df['daysOld'] / np.max(df['daysOld'])
     df['ageScore'] = df['ageScore'].replace(-np.Inf, 0)
 
-    # Create onehot encoding for the condition so we can use it as a feature as well
-    df = pd.concat([df, pd.get_dummies(df['condition'])], axis=1)
-
+    # Create a usefulCountClass column to treat usefulness prediction as a classification problem
+    if quantiles_for_class is not None:
+        buckets = get_buckets(df=df, quantiles=quantiles_for_class)
+        df['usefulCountClass'] = df['usefulCount'].apply(lambda row : assign_bucket(row, buckets))
 
     # Split data into train and val
     train = df.sample(frac=0.75, random_state=8)
@@ -70,3 +75,27 @@ def cap_col_val(val, usefulCount_range):
     if val > usefulCount_range[-1]:
         val = usefulCount_range[-1]
     return val
+
+
+def assign_bucket(val, buckets):
+    for i, bucket in enumerate(buckets):
+        if bucket[0] <= val < bucket[1]:
+            new_val = i
+    return new_val
+
+
+def get_buckets(df, quantiles):
+    cutoffs = []
+    buckets = []
+    for i in quantiles:
+        cutoffs.append(df['usefulCount'].quantile(q=i))
+    for i in range(1, len(cutoffs)):
+        if i == 1:
+            buckets.append([0, cutoffs[i-1]])
+        buckets.append([cutoffs[i-1], cutoffs[i]])
+        if i == len(cutoffs) - 1:
+            buckets.append([cutoffs[i], np.inf])
+    if len(cutoffs)==1:
+        buckets.append([0, cutoffs[0]])
+        buckets.append([cutoffs[0], np.inf])
+    return buckets
